e80e34fd6f3f7c2e5800a4041b38f3e3a8aa0e10
awais440 <43931297+awais440@users.noreply.github.com>
2018-10-19 21:23:41 +0500

    Add files via upload

---
+++ thread.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int array[1000];
+
+void * Add(void *arg){
+    int start = (int)arg;
+    int end = start+100;
+    int sum = 0;
+    for(int i = start;i < end;i++){
+        sum = sum+array[i];
+    }
+    return ((void*)sum);
+}
+
+int main(){
+    int status[10];
+    pthread_t thread[10];
+    int sum = 0;
+    for (int i = 0;i < 1000;i++){
+        array[i] = i+1;
+    }
+
+    for(int i = 0;i < 10;i++){
+        pthread_create(&thread[i],NULL,Add,(void*)(i*100));
+    }
+    
+    for(int i = 0;i < 10;i++){
+        pthread_join(thread[i],(void**) &status[i]);
+    }
+     
+    for(int i = 0;i < 10;i++){
+        sum = sum+status[i];
+    }
+    printf("sum is %d",sum);
+    
+    return 0;
+}
