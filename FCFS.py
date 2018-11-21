2be2f8c4cdadbc11ea3d92276118f0633bcfcdeb
awais440 <43931297+awais440@users.noreply.github.com>
2018-11-21 23:59:35 +0500

    Add files via upload

---
+++ FCFS.py
@@ -0,0 +1,36 @@
+process = []
+dictionary = {}
+parameters = []
+file = open("temp","r")
+line = file.readline()
+for word in line.split():
+    parameters.append(word)
+data = file.readlines()
+index = 0
+for line in data:
+    for word in line.split():
+        dictionary[parameters[index]] = word
+        index = index+1
+    process.append(dictionary.copy())
+    index = 0 
+import time
+process = sorted(process,key = lambda k: int(k['AT']))
+a = 0
+print("First Come First Serve")
+for dictionary in process:
+    if a < int(dictionary["AT"]):
+        print(a,end='')
+        while a < int(dictionary["AT"]):
+            print('.',end='')
+            a = a+1;  
+        print(a,end = '')
+    for i in range(1,int(dictionary["Duration"])+1):
+        print("_",end = '')
+    print(dictionary["Processes"],end = '')
+    print("(",end = '')
+    print(int(dictionary["Duration"])+a,end = '')
+    a = a+int(dictionary["Duration"])
+    print(")",end = '')
+    		
+
+        
